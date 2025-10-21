79194c9fe863d13a1f4e903d9d0fa0600c2ee36c
srsxnsh <srsxnsh@proton.me>
2025-10-21 13:05:58 +0100

    updates

--- 8ball.sh
+++ 8ball.sh
@@ -36,7 +36,7 @@ case "$1" in
     ;;
   shake)
 
-    defaults=("youre cooked lmao" "yeah maybe ig" "nah u fried fr" "yes" "no" "maybe" "perchance" "maychancehaps" "yeah probably tbh" "idk go gamble" "depends on what you think" "lmao who cares")
+    defaults=("youre cooked lmao" "yeah maybe ig" "nah u fried fr" "yes" "no" "maybe" "perchance" "maychancehaps" "yeah probably tbh" "idk go gamble" "depends on what you think" "lmao who cares" "certainly" "never")
     length=${#defaults[@]}
     index=$((RANDOM % length))
     echo "${defaults[$index]}"
